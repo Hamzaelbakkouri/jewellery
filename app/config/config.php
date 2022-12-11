bb788645136ccabfdac5ba6dbaeceb3af16e696f
hamzaelbakk <elbak.hamza.2018@gmail.com>
2022-12-11 17:37:48 +0100

    change

--- app/config/config.php
+++ app/config/config.php
@@ -2,6 +2,6 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/MVC-app-jewellery/jewellery');
+  define('URLROOT', 'http://localhost/jewellery_app/');
   // Site Name
   define('SITENAME', 'bijoux');
